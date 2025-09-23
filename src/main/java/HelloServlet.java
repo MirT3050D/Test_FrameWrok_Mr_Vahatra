7dec863e1f4b74b2603840947d4465376f51302d
MirT3050D <rahajarijaonamir@gmail.com>
2025-09-23 21:37:40 +0300

    workspace

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,22 @@
+package servlet;
+import java.io.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+
+    String message;
+
+    public void init() {
+        message = "Hello !";
+    }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+}
